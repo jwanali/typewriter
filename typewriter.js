ecd8670a9091f05bfc49a6d0bc14627305b5ba0f
jwanali <ciwan_ali@hotmail.com>
2023-05-19 11:01:08 -0500

    display sentence as animation

---
+++ typewriter.js
@@ -0,0 +1,17 @@
+const sentence = "hello there from lighthouse labs";
+let n = 0;
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+   
+  },n);
+
+  n += 50;
+
+}
+setTimeout(() => {
+  //console.log()
+  process.stdout.write('\n');
+},n)
+
+
